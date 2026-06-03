3593012e91a08395a6eb0473139dffeda297b1c8
vaishnavi-goyal <vaishnavigoel86@gmail.com>
2026-06-03 19:57:56 +0530

    Run main project

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-
-import streamlit as st
-
-st.title(" Salary Prediction App")
-
-name = st.text_input("Enter Job Title")
-
-experience = st.number_input("Experience", 0, 40, 1)
-
-if st.button("Predict"):
-    salary = 50000 + (experience * 5000)
-    st.success(f"Predicted Salary: ${salary}")
+import code
